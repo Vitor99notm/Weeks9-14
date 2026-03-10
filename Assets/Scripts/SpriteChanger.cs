01233d4422c687015b3c9873aac074561595464e
dasivito <dasilva.vitor.355@gmail.clom>
2026-03-10 11:46:47 -0400

    Started working on the SpriteChanger Script

---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public SpriteRenderer changeSprite;
+    public List<SpriteRenderer> spriteCounter;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSpriteChange(InputAction.CallbackContext context)
+    {
+
+    }
+}
