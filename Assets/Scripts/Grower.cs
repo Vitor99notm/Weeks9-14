e9d86ff12200d43b8f76effbba932983bad18115
Vitor99notm <dasilva.vitor.355@gmail.com>
2026-03-17 11:56:56 -0400

    More coding

--- Assets/Scripts/Grower.cs
+++ Assets/Scripts/Grower.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class Grower : MonoBehaviour
 {
     public Transform treeTransform;
+    public Transform appleTransform;
+    public float appleDelay = 1;
     void Start()
     {
         treeTransform.transform.localScale = Vector2.zero;
+        appleTransform.transform.localScale = Vector2.zero;
 
     }
 
@@ -25,11 +28,26 @@ public class Grower : MonoBehaviour
     {
         float t = 0;
 
-        while(t < 1){
+		treeTransform.transform.localScale = Vector2.zero;
+		appleTransform.transform.localScale = Vector2.zero;
+
+		while (t < 1){
 
             t += Time.deltaTime;
             treeTransform.transform.localScale = Vector3.one * t;
             yield return null;
         }
-    }
+
+        yield return new WaitForSeconds(appleDelay);
+
+        t = 0;
+
+		while (t < 1)
+		{
+
+			t += Time.deltaTime;
+			appleTransform.transform.localScale = Vector3.one * t;
+			yield return null;
+		}
+	}
 }
