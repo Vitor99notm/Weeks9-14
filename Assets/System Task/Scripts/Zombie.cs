609efa390b9e0db56851f484a39114e40a869e31
Vitor99notm <dasilva.vitor.355@gmail.com>
2026-04-05 15:19:23 -0400

    Zombie movement works

--- Assets/System Task/Scripts/Zombie.cs
+++ Assets/System Task/Scripts/Zombie.cs
@@ -2,28 +2,24 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
-    public Transform startPos;
+
+    //Keeps track of the players current position so that later code can use it to make zombie follow the player
     public Transform playerPos;
     public float speed = 0.1f;
     public float t;
 
-    public AnimationCurve curve;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
-        if (t > 3)
-        {
-            t = 0;
-        }
 
-        transform.position = Vector2.Lerp(startPos.position, playerPos.position, curve.Evaluate(t));
-
+        //Calculates the player position - the zombie position and makes the zombie follow the player
+		Vector2 direction = playerPos.position - transform.position;
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+        
+        
     }
 }
